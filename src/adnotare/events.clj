4c9acaf64bfe2f6d747c9c8533a61086d67b30bf
Artem Tseranu <git@arttsu.com>
2026-01-31 11:56:00 +0100

    Don't add empty annotation when nothing is selected

--- src/adnotare/events.clj
+++ src/adnotare/events.clj
@@ -16,9 +16,11 @@
                                                                  :selected-text selected-text})})
 
 (defmethod event-handler :adnotare/add-annotation [{:keys [fx/context adnotare/kind]}]
-  (let [{:keys [start end selected-text]} (subs/rich-area-selection context)
-        id (UUID/randomUUID)]
-    {:context (fx/swap-context context update-in [:annotations] assoc id {:start start :end end :kind kind :text selected-text})}))
+  (let [{:keys [start end selected-text]} (subs/rich-area-selection context)]
+    (if (.isEmpty selected-text)
+      {}
+      (let [id (UUID/randomUUID)]
+        {:context (fx/swap-context context update-in [:annotations] assoc id {:start start :end end :kind kind :text selected-text})}))))
 
 (defmethod event-handler :adnotare/select-annotation [{:keys [fx/context adnotare/id]}]
   {:context (fx/swap-context context assoc :selected-annotation-id id)})
